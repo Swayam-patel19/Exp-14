f48bcea25f189168926cce778c09ac0556454def
Swayam Patel <123808643+Swayam-patel19@users.noreply.github.com>
2025-04-24 23:06:43 +0530

    Add files via upload

---
+++ EXP14.cpp
@@ -0,0 +1,151 @@
+#14A
+# include <iostream>
+# include <string>
+using namespace std;
+
+class Uni
+{
+    public:
+    string uni= "Symbiosis";
+    void discipline()
+    {
+        cout << "Engineering" <<endl;
+    }
+};
+class Dep : public Uni
+{
+    public:
+    string dept= " Electronics & Telecommunication";
+};
+int main()
+{
+    Dep u1;
+    u1.discipline();
+    cout<<u1.uni + " "+u1.dept ;
+} 
+#14B
+#include<iostream> 
+#include<string> 
+using namespace std; 
+
+// Parent CLass1 
+class Vehicle {
+    public:
+    string company=" Ford";
+    void type(){
+        cout << "Mustang"<< endl;
+    }
+};
+// Parent Class 2
+class Specs{
+    public:
+    string mileage="8 kmpl";
+    void colour(){
+        cout<<"Grey"<<endl;
+    }
+};
+// Child Class-1 (derived from parent- 1&2)
+class Car: public Vehicle,public Specs{
+    public:
+    string seater = " 4 seater";
+};
+int main(){
+    //multiple inheritance
+    Car f2;
+    f2.colour();
+    cout<<f2.company<<" ";
+    f2.type();
+    cout<<"("<<f2.seater<<")"<<endl<<"MILEAGE:"<<f2.mileage<<endl;
+} 
+#14 C
+// Program to showmultilevel inheritance.
+
+#include<iostream> 
+#include<string>
+using namespace std; 
+
+// single base class
+class Student {
+public:
+    string stud;
+    void get_Student_data()
+    {
+        cout << "Enter the name of the student: ";
+        cin >>  stud;
+    }
+};
+ 
+// derived class from base class
+class PRN : public Student {
+public:
+    int prn;
+    void get_PRN_data()
+    {
+        cout << "Enter the PRN: ";
+        cin >> prn;
+    }
+};
+ 
+// derived from class derive1
+class Branch : public PRN {
+private:
+   string branch;
+ 
+public:
+    void get_Branch_data()
+    {
+        cout << "Enter the branch: ";
+        cin >> branch;
+    }
+ 
+    // function to print sum
+    void details()
+    {
+        cout << "Name: "<<stud<<"  PRN: "<< prn<<"  Branch: "<<branch << endl;
+    }
+};
+int main()
+{
+    // object of sub class
+    Branch obj;
+ 
+    obj.get_Student_data();
+    obj.get_PRN_data();
+    obj.get_Branch_data();
+    obj.details();
+    return 0;
+}
+#14 D
+
+
+// Program to show Hiererchical Inheritance.                    
+
+#include <iostream>
+using namespace std;
+
+// base class
+class Flower {
+public:
+    Flower() { cout << "This is a Flower. \n"; }
+};
+
+// first sub class
+class Lotus : public Flower {
+public:
+    Lotus() { cout << "This flower is a lotus. \n"; }
+};
+
+// second sub class
+class Marigold : public Flower {
+public:
+    Marigold() { cout << "Thisflower is a marigold. \n"; }
+};
+
+// main function
+int main()
+{
+    // Creating object of sub class will invoke the constructor of base class.
+    Lotus obj1;
+    Marigold obj2;
+    return 0;
+}
